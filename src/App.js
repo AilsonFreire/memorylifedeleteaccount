7f64d8344171f5e4cda662c5ce9e0c5f608998cf
Ailson Freire <ailson_freire@outlook.com>
2024-01-21 14:26:20 +0100

    creating the page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Como deletar sua conta no aplicativo <i>MemoryLife</i>
+      </h1>
+      <article style={{ marginTop: 50, textAlign: "left", marginLeft: 50 }}>
+        <lu>
+          <li>Para solicitar a deleção de sua conta o usario precisa estar logado</li>
+          <li>
+            Clicar na aba <b>Cofigurações</b> (Icone de engrenagem localizado no canto inferior direito)
+          </li>
+          <li>
+            Clicar no botão <b>Excluir minha conta</b>
+          </li>
+        </lu>
+        O processo de exclusão anonimisa os seguintes campos: Nome, Email e Registro Profissional do Cuidador responsável pela aplicação do aplicativo. O nome do paciente também será anonimisado, o que singfica que serão transformados em um camp hash que impossibilitára qualquer identificação
+        daqueles dados ao seu respectivo usuário de acordo com a LGPD. O aplicativo MemoryLife se reserva ao direito de manter os outros dados para fins de pesquisa e validação das funcionalidades do mesmo.
+      </article>
     </div>
   );
 }
